f11786d769dd4071c8b229c001176a2b0af9ec24
stoyalov.arseny <st.arseny@yandex.ru>
2022-06-04 19:45:21 +0300

    unparsable tables logged

--- src/main/scala/gp/tables/TablesStorage.scala
+++ src/main/scala/gp/tables/TablesStorage.scala
@@ -1,15 +1,19 @@
 package gp.tables
 
 import cats.Monad
-import cats.syntax.flatMap._
+import cats.data.OptionT
 import cats.effect.kernel.Async
+import cats.syntax.flatMap._
+import cats.syntax.traverse._
+import cats.syntax.functor._
+import cats.syntax.option._
 import doobie.implicits._
 import doobie.postgres.circe.json.implicits._
 import doobie.util.fragment.Fragment
 import doobie.util.transactor.Transactor
-import gp.utils.formats.postgres._
 import gp.tables.model.Table
 import gp.tables.model.formats.storage.StorageTable
+import gp.utils.formats.postgres._
 import tofu.logging.LoggingCompanion
 import tofu.syntax.logging._
 
@@ -27,7 +31,7 @@ object TablesStorage extends LoggingCompanion[TablesStorage] {
 
   lazy val tableName: String = "tables"
 
-  class Postgres[F[_]: Async: Monad: Log](implicit val transactor: Transactor[F]) extends TablesStorage[F] {
+  class Postgres[F[_]: Async: Log](implicit transactor: Transactor[F], F: Monad[F]) extends TablesStorage[F] {
     lazy val tableNameFragment: Fragment = Fragment.const(tableName)
 
     override def init(): F[Unit] =
@@ -49,8 +53,13 @@ object TablesStorage extends LoggingCompanion[TablesStorage] {
       (fr"select * from" ++ tableNameFragment ++ fr"where id = $id::uuid")
         .query[StorageTable]
         .option
-        .map(_.flatMap(_.asTable.toOption)) //todo error log
         .transact(transactor)
+        .flatMap { stOp =>
+          OptionT
+            .fromOption[F](stOp.map(s => OptionT(decodeStorageTable(s))))
+            .flatten
+            .value
+        }
 
     override def search(size: Option[Int], offset: Option[Int]): F[List[Table]] =
       (fr"select * from" ++ tableNameFragment ++ fr"limit ${size.getOrElse(10)} offset ${offset.getOrElse(0)}")
@@ -58,8 +67,17 @@ object TablesStorage extends LoggingCompanion[TablesStorage] {
         .stream
         .compile
         .toList
-        .map(_.flatMap(_.asTable.toOption)) //todo error log
         .transact(transactor)
+        .flatMap { stList =>
+          stList.traverse(decodeStorageTable).map(_.flatten)
+        }
+
+    private def decodeStorageTable(s: StorageTable): F[Option[Table]] =
+      s.asTable match {
+        case Right(t) => F.pure(t.some)
+        case Left(_) =>
+          error"Table ${s.id.toString} contains unprocessable column descriptions".as(none[Table])
+      }
   }
 
 }
